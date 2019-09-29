4ebd16bf965159686c73d35012651f0e42602286
zhangjiale <zhangjiale@btzh.cn>
2019-09-29 15:50:43 +0800

    区分api地址

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const hostname = location.hostname
 export let api = 'http://localhost:8080'
 
-if (hostname.includes('210yy.cn') || hostname.includes('149.248.10.24')) {
+if (hostname.includes('210yy.cn')) {
+  api = 'https://'+hostname
+} else if (hostname.includes('149.248.10.24')) {
   api = 'http://'+hostname
 }
